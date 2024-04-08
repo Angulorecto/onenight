972c0ab31f9a9551006eecc2d15fab5373b404d8
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2024-04-08 17:59:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
 const PORT = 3000;
 
 app.use(bodyParser.json());
 
 let nextRoomId = 1;
 const rooms = {};
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.post('/createRoom', (req, res) => {
   const roomId = nextRoomId++;
