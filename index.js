f34c61cb2e53e1e79a283365d262816498871b52
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2024-04-08 08:23:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function newStyle(styleurl) {
+  if (document.getElementById("style")) {
+    document.getElementById("style").remove();
+  }
   let style = document.createElement("link");
+  style.id = "style";
   style.rel = "stylesheet";
   style.href = styleurl;
+  document.getElementsByTagName("head")[0].appendChild(style);
 }
 function hostRoom() {
   
