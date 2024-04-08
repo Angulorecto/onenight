38fdd360a16b5ebb481184950da693bd48cab3f1
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2024-04-08 08:10:43 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+function newStyle(styleurl) {
+  let style = document.createElement("link");
+  style.rel = "stylesheet";
+  style.href = styleurl;
+}
+function hostRoom() {
+  
+}
+
+if ((localStorage.getItem("ingame") == false) or (localStorage.getItem("ingame") == null)) {
+  newStyle("styles/index.css");
+}
