13ed74bd942fbcb72924b4d49c50a88848d8ce6d
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2024-04-12 19:29:34 +0000

    modified:   public/host.html 	modified:   public/index.js 	modified:   server.js

--- public/index.js
+++ public/index.js
@@ -1,3 +1,13 @@
+// Function to get the value of a URL parameter by name
+function getUrlParameter(name) {
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+  var results = regex.exec(window.location.href);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 function loadHome() {
   let e = document.createElement("script");
   e.src = "https://cdn.socket.io/4.7.5/socket.io.min.js";
@@ -16,8 +26,18 @@ function loadHome() {
 
   document.getElementsByTagName("head")[0].appendChild(e);
 }
+
+function loadHost() {
+  // Get the value of the code parameter from the URL
+  var hostRoomCode = getUrlParameter('code');
+  // Update the element's innerHTML with the hostRoomCode value
+  document.getElementById("code").innerHTML = hostRoomCode;
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   if (window.location.pathname == "/") {
     loadHome();
+  } else if (window.location.pathname == "/host") {
+    loadHost();
   }
 });
--- server.js
+++ server.js
@@ -1,29 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const path = require('path');
-const bodyParser = require('body-parser');
-const PORT = 3000;
 
-app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const globalRoomCodes = new Set();
-
-app.use(express.static(path.join(__dirname, 'public'), { maxAge: 0 }));
-
-// Routes for serving HTML files
-app.get('/', (req, res) => {
-  const userAgent = req.headers['user-agent'];
-  const isMobile = /iPhone|iPad|iPod|Android|webOS|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
-  // Change stylesheet settings based on the result
-  if (isMobile) {
-    // For example, send a different HTML file for mobile devices
-    res.sendFile(path.join(__dirname, 'public', 'mobileIndex.html'));
-  } else {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  }
-});
 
 io.on("connection", (socket) => {
   socket.on("create room", () => {
@@ -45,51 +32,9 @@ function generateRoomCode() {
 
 app.get("/host", (req, res) => {
   const hostRoomCode = req.query.code;
-  res.sendFile(path.join(__dirname, 'public', 'host.html'));
+  res.sendFile(path.join(__dirname, 'public', 'host.html'), { hostRoomCode });
 });
 
-app.get('/room', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'room.html'));
-});
-
-// Create a new room and generate a room code
-app.post('/createRoom', (req, res) => {
-  const roomId = nextRoomId++;
-  const roomCode = generateRoomCode();
-  app.locals.rooms[roomCode] = { id: roomId, players: [] };
-  res.json({ roomCode });
-});
-
-// Join a room with a specific code
-app.post('/joinRoom/:code', (req, res) => {
-  const { code } = req.params;
-  if (app.locals.rooms[code]) {
-    const { playerName } = req.body;
-    addPlayerToRoom(code, playerName);
-    res.json({ success: true, playerId: app.locals.rooms[code].players[app.locals.rooms[code].players.length - 1].id });
-  } else {
-    res.json({ success: false });
-  }
-});
-
-function generateRoomCode() {
-  const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
-  let code = '';
-  for (let i = 0; i < 6; i++) {
-    code += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
-  return code;
-}
-
-function generatePlayerId() {
-  return Math.random().toString(36).substr(2, 9);
-}
-
-function addPlayerToRoom(roomCode, playerName) {
-  const player = { id: generatePlayerId(), name: playerName };
-  app.locals.rooms[roomCode].players.push(player);
-}
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+http.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
